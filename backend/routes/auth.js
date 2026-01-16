c848af3ed050fd3e9c97a50e7fdaa800ccede9ad
Aryan Bajpai <aryan.bajpai.cd.ece23@itbhu.ac.in>
2026-01-16 15:59:04 +0530

    renamed certain files

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-import { signupSchema, loginSchema } from "../validators/auth.js";
+import { signupSchema, loginSchema } from "../validators/zod.js";
 
 const router = express.Router();
 
