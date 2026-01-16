2a4d0416041baa37a9522f8eeec28086e5114db0
Aryan Bajpai <aryan.bajpai.cd.ece23@itbhu.ac.in>
2026-01-16 09:16:19 +0530

    frontend and backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);   
+})
+
