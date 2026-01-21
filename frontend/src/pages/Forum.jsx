811deb1f40495df327c53c4a3365eb2af81d9093
Aryan Bajpai <aryanbajpai499@gmail.com>
2026-01-21 18:10:07 +0530

    refactor: replace axios import with custom api module in Forum component

--- frontend/src/pages/Forum.jsx
+++ frontend/src/pages/Forum.jsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import ForumPostCard from "../components/ForumPostCard";
 import { useAuth } from "../context/AuthContext";
 import CreateForumPost from "../components/CreateForumPost";
-import axios from "axios";
+import api from "../api/axios.js";
 
 export default function Forum() {
   const { role } = useAuth() || {};
   const [posts, setPosts] = useState([]);
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:3000/api/forum/posts");
+    const res = api.get("/api/forum/posts");
     setPosts(Array.isArray(res.data) ? res.data : []);
   };
 
